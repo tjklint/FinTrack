140852cd1f04fd38caf030dfded831a70f1f3819
Joshua Kravitz <2271524@johnabbottcollege.net>
2023-12-09 14:48:56 -0500

    Added backing fields to Finances.cs and added Balance property.

--- Models/Finances.cs
+++ Models/Finances.cs
@@ -3,10 +3,26 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Finances
 {
+	private double _balance;
+	private double _expenses;
+	private double _income;
 
 	public Finances()
 	{
 		
+	}
+
+	public double Balance
+	{
+		get
+		{
+			return _balance;
+		}
+		set
+		{
+			//No need for validation as value can be a negative as they can be in debt.
+			_balance = value;
+		}
 	}
 
 }
