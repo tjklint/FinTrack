cddb5904fb1ecca126753488c499f5b9562dba8f
Joshua Kravitz <2271524@johnabbottcollege.net>
2023-12-27 15:20:38 -0500

    Created PayExpense.xaml and started to make the window.

---
+++ PersonalFinanceTracker.cs/Views/PayExpense.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonalFinanceTracker.cs.Views
+{
+    /// <summary>
+    /// Interaction logic for PayExpense.xaml
+    /// </summary>
+    public partial class PayExpense : Window
+    {
+        public PayExpense()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_PayExpense(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
